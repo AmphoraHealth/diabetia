7be9029e8fd2bfdfad64f0365d5cbb30781e5351
DanielS <daniel@amphorahealth.com>
2023-08-21 15:03:55 -0600

    fix: :zap: changes the knc library used

--- scripts4ml/aux_04_model_train/knc.py
+++ scripts4ml/aux_04_model_train/knc.py
@@ -1,5 +1,24 @@
-from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import faiss
 
-knc = KNeighborsClassifier(
-    n_neighbors = 5
+class FaissKNeighbors:
+    def __init__(self, k=5):
+        self.index = None
+        self.y = None
+        self.k = k
+
+    def fit(self, X, y):
+        self.index = faiss.IndexFlatL2(X.shape[1])
+        self.index.add(X.astype(np.float32))
+        self.y = y
+
+    def predict(self, X):
+        distances, indices = self.index.search(X.astype(np.float32), k=self.k)
+        votes = [self.y[_indices] for _indices in indices]
+        predictions = np.array([np.argmax(np.bincount(x)) for x in votes])
+        return predictions
+    
+
+knc = FaissKNeighbors(
+    k=5
 )
