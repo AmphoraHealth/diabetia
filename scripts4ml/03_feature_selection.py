5932e5974afa1e512abc2cec07260e60670e9b25
RafaPe <tinoco21.30@gmail.com>
2023-08-28 21:39:52 -0600

    refactor: :recycle: Changed code structure to work with auxiliary files

--- scripts4ml/03_feature_selection.py
+++ scripts4ml/03_feature_selection.py
@@ -20,6 +20,7 @@ sys.path.append(ROOT_PATH)
 from libs.global_constants import *
 from libs.logging import logging
 
+
 # Constants -------------------------------------------------------------------
 OUT_PATH = f"{S03_FEATURE_SELECTION}.json"
 DB_PATH = f"{S02B_STANDARDIZATION}.csv"
@@ -28,11 +29,12 @@ CONFIG_PATH = './conf/engineering_conf.json'
 definitions = json.load(open(f'{CONFIG_PATH}', 'r', encoding='UTF-8'))['config']['diagnosis']
 
 # Import libraries ------------------------------------------------------------
+from aux_03_feature_selection import methods
 import pandas as pd
 import json
-from sklearn.feature_selection import chi2
-from sklearn.naive_bayes import GaussianNB
-from sklearn.feature_selection import SequentialFeatureSelector
+# from sklearn.feature_selection import chi2
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.feature_selection import SequentialFeatureSelector
 
 # Code: feature selection -----------------------------------------------------
 # general code for feature selection given the selected FEATURE_SELECTION_METHOD
@@ -49,51 +51,21 @@ def get_fold_trainning(data:pd.DataFrame, folds_file:json, n_folds:int = 5) -> p
     train = pd.concat(train)
     return train
 
-def xi2_feature_selection(data:pd.DataFrame, label:pd.Series, n_features:int = 100) -> dict:
-    # get a list of columns with negative values
-    cols = [col for col in data.columns if data[col].min() < 0]
-    # add the minimum value to each column to make them all positive
-    data[cols] = data[cols] + abs(data[cols].min())
-    # get the best features
-    best_features = {}
-    chi, p_vals = chi2(data, label)
-    statistics = pd.DataFrame(zip(data.columns, chi, p_vals), columns = ['id', 'chi2', 'p-val']).sort_values('p-val')
-    statistics = statistics.head(n_features)
-    best_features['columns'] = list(statistics['id'])
-    best_features['chi2'] = list(statistics['chi2'])
-    best_features['p_val'] = list(statistics['p-val'])
-    return best_features
+def feature_selection(data:pd.DataFrame, label:pd.Series, n_features:int) -> json:
+    return methods[FEATURE_SELECTION_METHOD].fit(data, label, n_features)
 
-def probabilistic_feature_selection(data:pd.DataFrame, label:pd.Series, n_features:int = 100):
-    best_features = {}
-    naive_bayes = GaussianNB()
-    forward_selection = SequentialFeatureSelector(naive_bayes, n_features_to_select=n_features, direction="forward")
-    forward_selection.fit(data, label)
-    best_features['columns'] = list(data.columns[forward_selection.get_support()])
-    
-    return best_features
 
-def dummy_feature_selection(data:pd.DataFrame, label:pd.Series):
-    best_features = {}
-    best_features['columns'] = list(data.columns)
-
-    return best_features
 
 def main():
     logging.info('Reading data...')
-    data = pd.read_csv(f'{DB_PATH}', index_col = 0)
+    data = pd.read_csv(f'{DB_PATH}', index_col = 0, low_memory=False)
     data.drop('e11', axis = 1, inplace = True)
-    folds = json.load(open(f'{FOLD_PATH}', 'r', encoding='UTF-8'))
-    logging.info(f"Selecting fold {TEST_FOLD} as test and rest for feature selection")
-    data = get_fold_trainning(data, folds)
+    data.drop('dx_age_e11_cat', axis = 1, inplace = True)
+    columns_to_drop = [x for x in data.columns if 'label' in x]
+    data.drop(columns_to_drop, axis = 1, inplace = True)
     X, y = data.iloc[:,:-4], data[DIAGNOSTIC]
     logging.info(f"Starting feature selection process for {definitions[DIAGNOSTIC].replace('type_2_diabetes_mellitus', 'DM2').replace('_',' ')} using {FEATURE_SELECTION_METHOD} approach")
-    if FEATURE_SELECTION_METHOD == 'xi2':
-        features = xi2_feature_selection(X, y)
-    elif FEATURE_SELECTION_METHOD == 'probabilistic':
-        features = probabilistic_feature_selection(X, y, n_features = 3)
-    elif FEATURE_SELECTION_METHOD == 'dummy':
-        features = dummy_feature_selection(X, y)
+    features = feature_selection(X, y, n_features=100)
 
     with open(f'{OUT_PATH}', "w") as json_file:
         json.dump(features, json_file)
