0714d8178edb717980d320b42870114e0b04f003
DanielS <daniel@amphorahealth.com>
2023-08-31 17:13:25 -0600

    fix: :bug: convert columns to list

--- scripts4ml/aux_03_feature_selection/dummy.py
+++ scripts4ml/aux_03_feature_selection/dummy.py
@@ -3,5 +3,5 @@ import pandas as pd
 class DummyFeatureSelection:
     def fit(data:pd.DataFrame, label:pd.Series, n_features:int):
         best_features = {}
-        best_features['columns'] = data.columns
+        best_features['columns'] = list(data.columns)
         return best_features
