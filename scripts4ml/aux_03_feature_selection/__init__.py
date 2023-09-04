e8b67b1072dc28a934fba621540e53f9ce8036a8
JoaquinTripp <joaquin@amphorahealth.com>
2023-09-04 11:11:02 -0600

    feat: :sparkles: Clinical expertise feature selection added

--- scripts4ml/aux_03_feature_selection/__init__.py
+++ scripts4ml/aux_03_feature_selection/__init__.py
@@ -1,9 +1,11 @@
 from .dummy import DummyFeatureSelection
 from .xi2 import Xi2FeatureSelection
 from .forward_selection import ProbabilisticForwardSelection
+from .clinical_expertise import ClinicalExpertise
 
 methods:dict[str:object] = {
     'dummy': DummyFeatureSelection,
     'xi2': Xi2FeatureSelection,
-    'probabilistic':ProbabilisticForwardSelection
+    'probabilistic':ProbabilisticForwardSelection,
+    'clinical_expertise':ClinicalExpertise
 }
---
+++ scripts4ml/aux_03_feature_selection/clinical_expertise.py
@@ -0,0 +1,36 @@
+from sklearn.feature_selection import chi2
+import pandas as pd
+
+_features:list[str] = [
+    'cs_sex',
+    'age_at_wx',
+    'age_at_e11',
+    'years_since_dx',
+    'bmi_value',
+    'bmi_ordinal',
+    'diabetes_mellitus_type_2',
+    'essential_(primary)_hypertension',
+    'disorders_of_lipoprotein_metabolism_and_other_lipidemias',
+    'creatinine_value',
+    'creatinine_ordinal',
+    'glucose_value',
+    'glucose_ordinal',
+    'hemoglobin_value',
+    'hemoglobin_ordinal',
+    'triglycerides_value',
+    'triglycerides_ordinal',
+    'cholesterol_value',
+    'cholesterol_ordinal',
+    'type_2_diabetes_mellitus_with_renal_complications',
+    'type_2_diabetes_mellitus_with_ophthalmic_complications',
+    'type_2_diabetes_mellitus_with_neurological_complications',
+    'type_2_diabetes_mellitus_with_peripheral_circulatory_complications'
+]
+
+class ClinicalExpertise:
+    """This features were selected by a clinical expert"""
+    def fit(data, label, n_features):
+        # get the best features
+        best_features = {}
+        best_features['columns'] = _features
+        return best_features
